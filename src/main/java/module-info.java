f12abb00677d2c953e09506eb1fe048472e80b5c
Kamil Tomanek <kamilttt271@gmail.com>
2021-11-30 16:34:03 +0100

    Change in module-info.java 'requires lombok;'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.manhpd {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires lombok;
 
     opens com.manhpd to javafx.fxml;
     exports com.manhpd;
